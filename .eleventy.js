9100a7cdedec7167e74f8ed5a2707377a8e778ce
Andre Carvalhais <carvalhais@live.com>
2025-12-11 19:44:15 -0300

    feat: implement 'canonicalize' filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = (config) => {
+  config.addFilter("canonicalize", function(page) {
+    const site = this.ctx.site.url.replace(/\/$/, "");
+    const path = page.url || "/";
+    return site + path;
+  });
 	return {
 		dir: {
-			input: 'src',
-			output: 'dist',
+			input: "src",
+			output: "dist",
 		},
 	};
 };
